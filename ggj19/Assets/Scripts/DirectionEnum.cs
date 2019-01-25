d0ac4531ff4fef82de1b8260f14439a367a342b4
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2019-01-25 22:48:13 +0100

    create node and direction enum.

---
+++ ggj19/Assets/Scripts/DirectionEnum.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BrutalHack.ggj19
+{
+    public enum DirectionEnum
+    {
+        North,
+        South,
+        East,
+        West
+    }
+}
--- ggj19/Assets/Scripts/Node.cs
+++ ggj19/Assets/Scripts/Node.cs
@@ -3,21 +3,21 @@ using UnityEngine;
 
 namespace BrutalHack.ggj19
 {
-    public class GraphGenerator : MonoBehaviour
+    public class Node : MonoBehaviour
     {
-        public float worldRadius = 50f;
-        public float nodeDistance = 3f;
-        public List<Node> nodes;
-
+        public Vector3 coordinate;
+        public Dictionary<DirectionEnum, Node> neighbours;
+    
         // Start is called before the first frame update
         void Start()
         {
-            
+        
         }
 
         // Update is called once per frame
         void Update()
         {
+        
         }
     }
 }
