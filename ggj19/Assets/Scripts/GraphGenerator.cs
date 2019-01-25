d3f2408fa963d6e4777fd2d44de48c8186c1b1de
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2019-01-25 22:30:37 +0100

    add scripts folder, rider settings and empty GraphGeneration Script.

---
+++ ggj19/Assets/Scripts/GraphGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BrutalHack.ggj19
+{
+    public class GraphGenerator : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
