11f7ec933fcdc5769112401ececdad0f27d0268e
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2019-01-27 06:10:26 +0100

    test.

--- ggj19/Assets/Scripts/Tests/TestIsDead.cs
+++ ggj19/Assets/Scripts/Tests/TestIsDead.cs
@@ -1,5 +1,6 @@
 using BrutalHack.ggj19.General;
 using NUnit.Framework;
+using UnityEngine;
 
 namespace BrutalHack.ggj19.Tests
 {
@@ -9,8 +10,18 @@ namespace BrutalHack.ggj19.Tests
         [Test]
         public void Test()
         {
-            NodeCollectionLogic nodeCollectionLogic = null;
-            Assert.AreEqual(0, 0);
+            NodeCollectionLogic nodeCollectionLogic = NodeCollectionLogic.Instance;
+            Node from = CreateNode(new Vector2(0, 0));
+            Node to = CreateNode(new Vector2(0, 1));
+            nodeCollectionLogic.TrackAndHandleMove(from, to);
+
+            Assert.AreEqual(2, nodeCollectionLogic.passedNodes.Count);
+            Assert.AreEqual(1, nodeCollectionLogic.deadNodes.Count);
+        }
+
+        private Node CreateNode(Vector2 coordinate)
+        {
+            return new Node {Coordinate = coordinate};
         }
     }
 }
