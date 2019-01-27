7974642dcc3fb25f1b95ee11e66cd12b52687cba
Benjamin Justice <benjamin.justice@brutalhack.com>
2019-01-27 15:27:22 +0100

    Added outro camera and music

--- ggj19/Assets/Scripts/General/CameraEndgameTrigger.cs
+++ ggj19/Assets/Scripts/General/CameraEndgameTrigger.cs
@@ -10,12 +10,21 @@ namespace BrutalHack.ggj19
         public PlayerManager playerManager;
 
         private static readonly int End = Animator.StringToHash("end");
+        public AudioClip outroClip;
 
         // Start is called before the first frame update
         void Start()
         {
-            musicController.OnSongFinished += () => GetComponent<Animator>().SetTrigger(End);
-            playerManager.OnEndGame += () => GetComponent<Animator>().SetTrigger(End);
+//            musicController.OnSongFinished += EndGameCameraAndVoice;
+            playerManager.OnEndGame += EndGameCameraAndVoice;
+        }
+
+        private void EndGameCameraAndVoice()
+        {
+            GetComponent<Animator>().SetTrigger(End);
+            AudioSource audioSource = musicController.GetComponent<AudioSource>();
+            audioSource.clip = outroClip;
+            audioSource.Play();
         }
     }
 }
